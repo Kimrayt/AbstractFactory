b8191e956207894c5b4b9ce7fc2393749e5d0071
Kimrayt <103435959+Kimrayt@users.noreply.github.com>
2022-04-16 17:41:59 +0300

    Update Main.java
    Елисей, прошу просмотреть на строки 27, 34 и 41 в Main. При определении Knife k, Pot p etc я получаю ошибку, что эта переменная уже объявлена. Однако если я не объявляю их в 14-16 строках, то получаю ошибку в 55, 58, 61, 74, 77, 90, 93, так как присвоение значения переменной находится по условием. Соответственно, вопрос. Можно ли как-то принудительно заставить запуститься или тут есть какая-то хитрость, до меня не доходящая?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+
     while (money >0){
+        Knife k = null;
+        Pot p = null;
+        HoningStone h = null;
         System.out.println("Your current belongings is "+ money);
         System.out.println("What you would like to buy? " + "1. Knife " + "2. Pot " + "3. Honing stone");
         Scanner in = new Scanner (System.in);
@@ -16,6 +22,7 @@ public class Main {
         int man = in.nextInt();
         KitchenwareFactory creator;
         String product = null;
+
         creator = man == 1? new SamuraFactory(): new CoolinaFactory();
             if (type == 1) {
                 Knife k = creator.createKnife();
@@ -38,26 +45,57 @@ public class Main {
                 product = "honing stone";
 
             }
-        if (product == "knife"){
+        if (Objects.equals(product, "knife")){
             System.out.println("What you would like to do with a knife? 1. ");
-
-        }
-        else if (product == "pot"){
             boolean end = false;
-            while (end == false) {
+            while (!end) {
+                System.out.println("What you would like to do with a knife? "+ "1. Test sharpness " + "2. Cut "+ "3. Sharpen");
+                int a = in.nextInt();
+                if (a == 1){
+                    k.testSharpness();
+                }
+                else if (a == 2){
+                    k.cut();
+                }
+                else if (a == 3){
+                    k.sharpen();
+                }
+                System.out.println("Would you like to continue playing with your purchase? Y/N");
+                String b = in.nextLine();
+                end = b == "N"? true:false;
+            }
+        }
+        else if (Objects.equals(product, "pot")){
+            boolean end = false;
+            while (!end) {
                 System.out.println("What you would like to do with a pot? "+ "1. Boil something" + "2. Put it on");
                 int a = in.nextInt();
                 if (a == 1){
-                p.boilSometing;
+                p.boilSomething();
                 }
                 else if (a == 2){
-                p.putOn;
+                p.putOn();
                 }
+                System.out.println("Would you like to continue playing with your purchase? Y/N");
+                String b = in.nextLine();
+                end = b == "N"? true:false;
             }
         }
-        else if (product == "honing stone"){
-            System.out.println("What you would like to do with a honing stone?");
-
+        else if (Objects.equals(product, "honing stone")){
+            boolean end = false;
+            while (!end){
+            System.out.println("What you would like to do with a honing stone? "+ "1. Hone knife " + "2. Hone pot ");
+                int a = in.nextInt();
+                if (a == 1){
+                    h.honeKnife();
+                }
+                else if (a == 2){
+                    h.honePot();
+                }
+                System.out.println("Would you like to continue playing with your purchase? Y/N");
+                String b = in.nextLine();
+                end = b == "N"? true:false;
+            }
         }
     }
     }
